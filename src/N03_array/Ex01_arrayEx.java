b22566327147a152c08618d3b4f75624d1a388ea
KrillM <mks160615@gmail.com>
2024-01-18 15:11:37 +0900

    array practice through for

---
+++ src/N03_array/Ex01_arrayEx.java
@@ -0,0 +1,11 @@
+package N03_array;
+
+public class Ex01_arrayEx {
+    public static void main(String[] args) {
+        int[]x;
+        x=new int[]{1,2,3,4,5}; // 아싸 이 이슈 해결!
+
+        for(int i: x)
+            System.out.print(i+" ");
+    }
+}
---
+++ src/N03_array/Q01_arrayAvg.java
@@ -0,0 +1,22 @@
+package N03_array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q01_arrayAvg {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[]x=new int[5];
+
+        int total=0;
+        for(int i=0;i<x.length;i++){
+            x[i]=sc.nextInt();
+            total += x[i];
+        }
+        double avg = (double) total/x.length;
+
+        System.out.println(Arrays.toString(x));
+        System.out.printf("평균 %.1f", avg);
+    }
+}
